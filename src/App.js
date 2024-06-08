58efa2fd80fc0c2e2fd25013999d691242f484ef
Marissa Hagglund <mdhagglund@gmail.com>
2024-06-08 10:36:28 -0700

    initialize project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+         Red Pandas are the best
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div>
+         Red Pandas are the best
+        </div>
+    );
+}
+
+export default Home;
